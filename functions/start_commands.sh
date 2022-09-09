ed6fb8db63f61e928d6b73303103513cc1a25411
Drashna Jael're <drashna@live.com>
2022-09-08 18:02:07 -0700

    Add script files

---
+++ functions/start_commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo echo "sudoed"
+sudo steamos-readonly disable
+
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
---
+++ functions/stop_commands.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo echo "sudoed"
+
+yes | sudo pacman -Scc
+sudo steamos-readonly enable
